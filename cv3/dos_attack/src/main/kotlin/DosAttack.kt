5467df33cc3658a14c2a0bb51ef2de5a219779cf
Filip Peterek <filip.peterek@firma.seznam.cz>
2020-11-27 21:34:07 +0100

    Code cleanup

--- cv3/dos_attack/src/main/kotlin/DosAttack.kt
+++ cv3/dos_attack/src/main/kotlin/DosAttack.kt
@@ -1,15 +1,11 @@
 package org.fpeterek.dos
 
 import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
 
 object DosAttack {
 
     private val isRunningValue = AtomicBoolean(false)
-
-    private val connCounter = AtomicInteger(0)
-    private val outputConnCount = AtomicBoolean(true)
 
     private var isRunning
         get() = isRunningValue.get()
@@ -17,21 +13,11 @@ object DosAttack {
 
     private var threads = listOf<Thread>()
 
-    val connections
-        get() = connCounter.get()
-
     private val callerThread
         get() = thread {
-
             while (isRunning) {
                 try {
-                    val req = HttpClient.get("127.0.0.1", 8080)
-                    val conns = connCounter.incrementAndGet()
-                    req.close()
-                    if (!req.isSuccessful && outputConnCount.get()) {
-                        println("Connections created: $conns")
-                        outputConnCount.set(false)
-                    }
+                    HttpClient.get("127.0.0.1", 8080)
                 } catch (e: Exception) {
                     // noop
                 }
